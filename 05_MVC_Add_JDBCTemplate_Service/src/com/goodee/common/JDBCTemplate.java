1f1069066d96a579bbe3e48371f09533c312f4e6
gdu-java <sunnykid7@naver.com>
2023-04-26 12:31:15 +0900

    0426-1231

--- 05_MVC_Add_JDBCTemplate_Service/src/com/goodee/common/JDBCTemplate.java
+++ 05_MVC_Add_JDBCTemplate_Service/src/com/goodee/common/JDBCTemplate.java
@@ -2,7 +2,9 @@ package com.goodee.common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCTemplate {
 	// 모든 메서드를 static 으로 생성
@@ -27,6 +29,7 @@ public class JDBCTemplate {
 		return conn;
 	}
 	
+	//2. commit을 처리해 주는 메서드(Connection객체 전달받아야함.)
 	public static void commit(Connection conn) {
 		try {
 			if(conn != null && !conn.isClosed()) {
@@ -37,6 +40,7 @@ public class JDBCTemplate {
 		}
 	}
 	
+	//3. rollback을 처리해주는 메서드(Connection객체 전달받아야함.)
 	public static void rollback(Connection conn) {
 		
 		try {
@@ -47,6 +51,44 @@ public class JDBCTemplate {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	//4. JDBC용 객체들 전달받아서 반납처리해주는 메서드
+	//1) Statement관련 객체를 전달받아서 반납시켜주는 메서드
+	//   Statement와 PreparedStatement는 둘 다 Statement로 받을 수 있음.
+	public static void close(Statement stmt) { 
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//2) ResultSet객체 전달받아서 반납해주는 메서드 :  메서드 오버로딩
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	//3) Connection객체 전달받아서 반납해주는 메서드 : 메서드 오버로딩
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
 	}
 	
 }
